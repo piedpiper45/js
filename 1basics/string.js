5bb37103dbdb3e5f87815fdb1ef654e756bada37
Mrutyunjay S Hiremath <59961701+piedpiper45@users.noreply.github.com>
2023-09-02 11:20:23 +0000

    stringsdone

---
+++ 1basics/string.js
@@ -0,0 +1,29 @@
+const nameis="muttu";
+const repcount=100;
+//log(nameis+repcount+" lipm");//outdated 
+console.log(`my name is ${nameis} and my repcount is ${repcount}`);
+
+const g=new String('abcdefghowjoiewrewp');
+//console.log(g[0]);
+/*
+console.log(g.__proto__);
+console.log(g.toUpperCase());
+console.log(g.charAt(2));
+console.log(g.indexOf('u'));
+
+const m=g.substring(1,4);
+console.log(`newsubstr: ${m}`);
+const k=g.slice(-1,5);
+console.log(`slicing ${k}`);*/
+const h="   hhshs ";
+console.log(h);
+console.log(h.trim()) // remoes unnecessary chars like space
+
+const url="https://muttub=hiremath@20pion.com"
+
+console.log(url.replace('@20',"**"));
+console.log(url.includes('muttu'));
+
+
+const a="hut-net-com"
+console.log(a.split('-'));
