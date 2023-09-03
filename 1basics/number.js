5277f04129a6179d855708f2e67de732fe61d31d
Mrutyunjay S Hiremath <59961701+piedpiper45@users.noreply.github.com>
2023-09-03 08:15:03 +0000

    numbers done

---
+++ 1basics/number.js
@@ -0,0 +1,20 @@
+
+const n=new Number(45.45);
+
+// console.log(n); //object number
+
+// console.log(n.toFixed(4)); // how many decimal digits u want
+// let a=1000000;
+// console.log(a.toLocaleString('en-IN')); // indian commas rupees (default usa)4
+// a=43.856
+// console.log(`prec ${a.toPrecision(2)}`);// makes only 2digit-->44
+// console.log( a.toString());
+
+// //*****Maths*****
+// console.log(Math);//math object
+// console.log(Math.abs(-88));
+
+console.log(Math.random());// between 0 to 1
+min=10
+max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
