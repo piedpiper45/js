8e2b93dd5bd6e5b938e237d4f3402908952fe76d
Mrutyunjay S Hiremath <59961701+piedpiper45@users.noreply.github.com>
2023-09-03 12:51:21 +0000

    dateconcept done

---
+++ 1basics/date.js
@@ -0,0 +1,22 @@
+
+// let date=new Date();
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+
+// let mydate=new Date(2023,0,1);
+// let mydate=new Date(2023,0,1,5,3);
+// let month=1
+// let date=14
+// // let year=2001
+let mydate=new Date("01-14-2001");
+// console.log(mydate.toLocaleString());
+
+// let mytimestamp=Date.now();
+// // console.log(mytimestamp);
+// // console.log(mydate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+console.log(mydate.getDate());
+console.log(mydate.getDay());
+console.log(mydate.getHours());
